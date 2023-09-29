ae042cd51c0badbf58b93fe0a226551b5ee1aefd
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-09-29 23:28:47 +0200

    Replace flexbox with grid

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,65 +1,21 @@
-(()=>{
-    function togglePause() {
-        var myPlayer = videojs("my-video");
-        if (myPlayer.paused()) {
-            myPlayer.play();
-        }
-        else {
-            myPlayer.pause();
-        }
-    }
-    
-    document.getElementById("my-video").addEventListener("click", togglePause);
-
-//slider
-const swiper = new Swiper("#swiper1", {
-    grabCursor: true,
-    effect: "creative",
-    creativeEffect: {
-        prev: {
-            shadow: true,
-            translate: [0, 0, -400],
-        },
-        next: {
-            translate: ["100%", 0, 0],
-        },
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
-
-
 $(function () {
-    $('[data-fancybox]').fancybox({
-        iframe : {
-            css : {
-                width : '600px',
-                height : '400px'
-            }
-        }
-    });
-});
-
-
-  
-
-//slider mag
-
-
-  const swiper2 = new Swiper('.swiper-container', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  $("[data-fancybox]").fancybox({
+    iframe: {
+      css: {
+        width: "600px",
+        height: "400px",
+      },
     },
-    spaceBetween: 30, /* l'espace entre les images dans le swiper */
-    slidesPerView: 'auto', /* pour ajuster automatiquement le nombre d'images à afficher */
   });
-  
-console.log('Done');
 
+  function togglePause() {
+    var myPlayer = videojs("my-video");
+    if (myPlayer.paused()) {
+      myPlayer.play();
+    } else {
+      myPlayer.pause();
+    }
+  }
 
-})();
-
+  document.getElementById("my-video").addEventListener("click", togglePause);
+});
