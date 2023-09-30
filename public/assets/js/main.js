55ef5e3c59d028446e39f369d522b9450c5b69c0
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-09-30 21:10:33 +0200

    Add multiple submenu panel manager for burger menu

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20,9 +20,12 @@ $(function () {
   document.getElementById("my-video").addEventListener("click", togglePause);
 
   const sidenav = document.querySelector("[data-sidenav]");
+  const sidenavMenuContent = document.querySelector("[data-sidenav-core]");
+
   const burgerOpenBtn = document.querySelector("[data-burger-open-btn]");
   const burgerCloseBtn = document.querySelector("[data-burger-close-btn]");
-  const submenuTriggerBtn = document.querySelector("[data-submenu-root]");
+  const submenuTriggerBtn = document.querySelectorAll("[data-submenu-root]");
+  const submenuSidenav = document.querySelector("[data-sidenav-submenu]");
 
   burgerOpenBtn.addEventListener("click", () => {
     sidenav.classList.add("active")
@@ -31,10 +34,19 @@ $(function () {
   
   burgerCloseBtn.addEventListener("click", () => {
     sidenav.classList.remove("active")
+    // sidenavMenuContent.classList.remove("hide")
     document.body.classList.remove("sidemenu-opened")
   })
 
-  submenuTriggerBtn.addEventListener("click", () => {
-    
-  })
+  submenuTriggerBtn.forEach(element => {
+    element.addEventListener("click", (e) => {
+      const submenuData = e.target.dataset.submenuRoot;
+      const submenuTitle = e.target.textContent
+      const submenuLevel = Number(e.target.dataset.submenuLevel);
+
+      sidenavMenuContent.style.transform = `translateX(-${submenuLevel * 100}%)`;
+      console.log(sidenavMenuContent)
+      submenuSidenav.querySelector("h1").textContent = submenuTitle
+    })
+  });
 });
