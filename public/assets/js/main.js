9ed32167bbc43aba6edeb8fc2e1914c66d71016e
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-09-30 22:06:44 +0200

    Manage entries types for burger menu sub levels

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -24,29 +24,73 @@ $(function () {
 
   const burgerOpenBtn = document.querySelector("[data-burger-open-btn]");
   const burgerCloseBtn = document.querySelector("[data-burger-close-btn]");
+  const burgerReturnBtn = document.querySelector("[data-burger-return-btn]");
   const submenuTriggerBtn = document.querySelectorAll("[data-submenu-root]");
   const submenuSidenav = document.querySelector("[data-sidenav-submenu]");
 
-  burgerOpenBtn.addEventListener("click", () => {
-    sidenav.classList.add("active")
-    document.body.classList.add("sidemenu-opened")
-  })
-  
-  burgerCloseBtn.addEventListener("click", () => {
-    sidenav.classList.remove("active")
-    // sidenavMenuContent.classList.remove("hide")
-    document.body.classList.remove("sidemenu-opened")
-  })
+  const submenuPanelTpl = document.querySelector("[data-template-id=submenu-panel]");
+  const submenuPanelButton = document.querySelector("[data-template-id=submenu-button]");
+  const submenuPanelLink = document.querySelector("[data-template-id=submenu-link]");
+  let currentSubMenuLevel = 1
 
-  submenuTriggerBtn.forEach(element => {
-    element.addEventListener("click", (e) => {
-      const submenuData = e.target.dataset.submenuRoot;
-      const submenuTitle = e.target.textContent
+  burgerOpenBtn.addEventListener("click", () => {
+    sidenav.classList.add("active");
+    document.body.classList.add("sidemenu-opened");
+  });
+
+  burgerCloseBtn.addEventListener("click", () => {
+    sidenav.classList.remove("active");
+    // sidenavMenuContent.classList.remove("hide")
+    document.body.classList.remove("sidemenu-opened");
+  });
+
+  burgerReturnBtn.addEventListener("click", () => {
+    currentSubMenuLevel--
+    sidenavMenuContent.style.transform = `translateX(-${currentSubMenuLevel * 100}%)`;
+  });
+
+  const generateSubmenuPanel = (e) => {
+    const submenuDataRaw = e.target.dataset.submenuRoot;
+      const submenuData = JSON.parse(submenuDataRaw)
+      const submenuTitle = e.target.textContent;
       const submenuLevel = Number(e.target.dataset.submenuLevel);
 
-      sidenavMenuContent.style.transform = `translateX(-${submenuLevel * 100}%)`;
-      console.log(sidenavMenuContent)
-      submenuSidenav.querySelector("h1").textContent = submenuTitle
-    })
+      sidenavMenuContent.style.transform = `translateX(-${
+        currentSubMenuLevel * 100
+      }%)`;
+
+      console.log(submenuData);
+      submenuSidenav.querySelector("[data-title]").textContent = submenuTitle;
+      
+      submenuData.forEach((item) => {
+        const li = document.createElement("li");
+        let tpl = submenuPanelLink.content.cloneNode(true)
+
+        if ("sub_menu" in item) {
+          tpl = submenuPanelButton.content.cloneNode(true)
+          const button = tpl.querySelector("button")
+          var text = document.createTextNode(item.name);
+          button.prepend(text);
+          button.dataset.submenuRoot = JSON.stringify(item.sub_menu)
+          button.addEventListener("click", generateSubmenuPanel);
+        } else {
+          const link = tpl.querySelector("a")
+          link.textContent = item.name
+          link.setAttribute('href', item.link);
+        }
+
+        // submenuPanelButton
+        // const button = document.createElement("button");
+        // button.textContent = item.name
+
+        li.appendChild(tpl);
+        submenuSidenav.querySelector("[data-list-items]").appendChild(li);
+      })
+
+      currentSubMenuLevel++
+  }
+
+  submenuTriggerBtn.forEach((element) => {
+    element.addEventListener("click", generateSubmenuPanel);
   });
 });
